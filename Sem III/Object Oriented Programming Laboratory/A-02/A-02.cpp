968123f5ad16f4544b5db50d96cfa5f2826c25af
bhosalepranil16 <bhosalepranil16@gmail.com>
2022-11-27 17:12:46 +0530

    OOP PDF added

---
+++ Sem III/Object Oriented Programming Laboratory/A-02/A-02.cpp
@@ -0,0 +1,9 @@
+// Problem Statement:
+// Write a C++ program create a calculator for an arithmetic operator (+, -, *, /). The
+// program should take two operands from user and performs the operation on those two
+// operands depending upon the operator entered by user. Use a switch statement to select
+// the operation. Finally, display the result.
+
+#include<iostream>
+using namespace std;
+
