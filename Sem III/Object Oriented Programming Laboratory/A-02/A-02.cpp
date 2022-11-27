dbd35c8fbc38e88893c72782cc083d96bd4af0db
bhosalepranil16 <bhosalepranil16@gmail.com>
2022-11-27 17:37:10 +0530

    OOP A-02 added

--- Sem III/Object Oriented Programming Laboratory/A-02/A-02.cpp
+++ Sem III/Object Oriented Programming Laboratory/A-02/A-02.cpp
@@ -4,6 +4,56 @@
 // operands depending upon the operator entered by user. Use a switch statement to select
 // the operation. Finally, display the result.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+class Calculator {
+    float num1, num2;
+    public:
+    Calculator() {
+        cout << "Enter num1: ";
+        cin >> num1;
+        cout << "Enter num2: ";
+        cin >> num2;
+    }
+    void addition() {
+        cout << "Addition: " << num1 + num2 << "\n";
+    }
+    void subtraction() {
+        cout << "Subtraction: " << num1 - num2 << "\n";
+    }
+    void multiplication() {
+        cout << "Multiplication: " << num1 * num2 << "\n";
+    }
+    void division() {
+        cout << "Division: " << num1 / num2 << "\n";
+    }
+};
+
+int main() {
+    Calculator Obj;
+    char choice;
+    do {
+        cout << "Enter Operator(+, -, *, /): ";
+        cin >> choice;
+
+        switch (choice) {
+            case '+':
+                Obj.addition();
+                break;
+            case '-':
+                Obj.subtraction();
+                break;
+            case '*':
+                Obj.multiplication();
+                break;
+            case '/':
+                Obj.division();
+                break;
+            default:
+                cout << "Invalid Choice\n";
+                break;
+        }
+    } while (choice != '0');
+    return 0;
+}
